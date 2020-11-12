262c38e208b122dce01f435ab34a8268156c23cc
Patrick Zhang <zhangpatrick2004@gmail.com>
2020-11-12 12:35:30 -0500

    Update boxmovement.cs

--- ADSB-Project/Assets/boxmovement.cs
+++ ADSB-Project/Assets/boxmovement.cs
@@ -8,7 +8,6 @@ public class boxmovement : MonoBehaviour
 	public float jumpHeight = 1000.0f;
 	public float playerspeed = 10.0f;
 	public Rigidbody body;
-	private grounded = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,25 +17,13 @@ public class boxmovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(playerspeed*Input.GetAxis("Horizontal")*Time.deltaTime, 0, playerspeed*Input.GetAxis("Vertical")*Time.deltaTime);
+        Vector3 move = new Vector3(playerspeed*Input.GetAxis("Horizontal")*Time.deltaTime, Input.GetAxis("Jump"), playerspeed*Input.GetAxis("Vertical")*Time.deltaTime);
 		if (move != Vector3.zero)
         {
             transform.position = transform.position + move;
 			
         }
-		if (Input.GetKey("space") && grounded) 
-		{
-			body.AddForce(0, jumpHeight*Time.deltaTime, 0);
-			Debug.Log("jumped");
-		}
     }
-	
-	void OnCollisionEnter(Collision collision) 
-	{
-		if (collision.gameObject.name == "Terrain") 
-		{
-			grounded = true;
-		}
-	}
+
 		 
 }
