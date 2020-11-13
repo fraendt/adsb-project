5bf822b3e4785a8c8118ca811d81b425c3af94ce
Patrick Zhang <zhangpatrick2004@gmail.com>
2020-11-12 22:11:06 -0500

    big body changes
    added head, lengthened body, head movement, body movement relative to camera direction, lost the ability to jump

---
+++ ADSB-Project/Assets/bodymove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bodymove : MonoBehaviour
+{
+
+	public float jumpHeight = 500.0f;
+	public float playerspeed = 10.0f;
+	public Rigidbody body;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(playerspeed*Input.GetAxis("Horizontal")*Time.deltaTime, Input.GetAxis("Jump"), playerspeed*Input.GetAxis("Vertical")*Time.deltaTime);
+		if (move != Vector3.zero)
+        {
+            //transform.position = transform.position + move;
+			//transform.Translate(move, Space.World);
+			//transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(move), 0.15F);
+			//camera forward and right vectors:
+			var camera = Camera.main;
+			
+			var forward = camera.transform.forward;
+			var right = camera.transform.right;
+	 
+			//project forward and right vectors on the horizontal plane (y = 0)
+			forward.y = 0f;
+			right.y = 0f;
+			forward.Normalize();
+			right.Normalize();
+	 
+			//this is the direction in the world space we want to move:
+			var desiredMoveDirection = forward * Input.GetAxis("Vertical") + right * Input.GetAxis("Horizontal");
+	 
+			//now we can apply the movement:
+			transform.Translate(desiredMoveDirection * playerspeed * Time.deltaTime);
+			
+        }
+		
+    }
+
+		 
+}
---
+++ ADSB-Project/Assets/headmove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class headmove : MonoBehaviour
+{
+	public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+	
+	public Transform player; 
+	private Vector3 offset = new Vector3(0,2,0);
+	
+	public float sensitivity = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X") * sensitivity;
+        pitch -= speedV * Input.GetAxis("Mouse Y") * sensitivity;
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+		
+		transform.position = player.position + offset;
+    }
+}
