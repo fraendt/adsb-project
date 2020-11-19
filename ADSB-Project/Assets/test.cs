1cb7dbc77ae3db19b89e31308eeeb3c6defcf2a0
Patrick Zhang <zhangpatrick2004@gmail.com>
2020-11-19 12:54:24 -0500

    minor changes
    created sprint1 binary

---
+++ ADSB-Project/Assets/test.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class test : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+    private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // controller.Move(move * Time.deltaTime * playerSpeed);
+		
+
+        if (move != Vector3.zero)
+        {
+            //gameObject.transform.forward = move;
+			Camera.current.transform.Translate(move);
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+	/*
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal");
+		float zAxisValue = Input.GetAxis("Vertical");
+		if(Camera.current != null)
+		{
+			// Console.WriteLine(xAxisValue, zAxisValue);
+			
+		}
+    }*/
+}
